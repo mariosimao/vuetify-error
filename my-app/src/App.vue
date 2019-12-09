32d55144fc69f972d600c125474263867b57d046
Mario Gunter Simao <mario.simao@cern.ch>
2019-12-09 22:20:58 +0100

    Initial commit

---
+++ my-app/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <my-sidebar/>
+  </v-app>
+</template>
+
+<script>
+// eslint-disable-next-line import/no-extraneous-dependencies
+import MySidebar from 'my-lib/src/components/MySidebar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    MySidebar,
+  },
+};
+</script>
---
+++ my-lib/src/components/MySidebar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <v-navigation-drawer app v-model="drawer">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MySidebar',
+  data: () => ({
+    drawer: null,
+  }),
+};
+</script>
